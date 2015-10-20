a0be1f3f3348b0e2dd831620cdfd1510983a8b56
Alexander <zhokin@mail.cern.ch>
2015-10-20 09:40:59 +0200

    AZ 20.10.2015 debugging from Andrius

--- RecoHcal/HcalPromptAnalysis/test/SHIFTER_VALIDATION/GLOBAL_EOS.sh
+++ RecoHcal/HcalPromptAnalysis/test/SHIFTER_VALIDATION/GLOBAL_EOS.sh
@@ -3,6 +3,7 @@
 WebDir='/store/group/dpg_hcal/comm_hcal/www/HcalRemoteMonitoring'
 WebSite='https://cms-conddb-dev.cern.ch/eosweb/hcal/HcalRemoteMonitoring'
 HistoDir='/store/group/dpg_hcal/comm_hcal/www/HcalRemoteMonitoring/CMT/histos'
+eos='/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select'
 
 # print usage info
 if [[ "$1" == "" ]]; then
@@ -13,19 +14,52 @@ if [[ "$1" == "" ]]; then
   exit
 fi
 
-cmsenv
+cmsenv 2>/dev/null
+if [ $? == 0 ] ; then
+    eval `scramv1 runtime -sh`
+fi
+echo "using eos command `ls ${eos}`"
 
-k=0
+
+debug=0
+DEBUG_DIR="dir-debug/"
+if [ "$1" == "-debug2" ] ; then
+    #echo "-debug2 detected"
+    debug=2
+    shift 1
+elif [ "$1" == "-debug" ] ; then
+    #echo "-debug detected"
+    debug=1
+    #rm -rf ${DEBUG_DIR}/*
+    if [ ! -d ${DEBUG_DIR} ] ; then mkdir ${DEBUG_DIR}; fi
+    shift 1
+fi
+if [ ${debug} -gt 0 ] ; then
+    echo -e "\n\tDEBUG MODE=${debug} detected\n"
+fi
+
+runList=""
+if [ ${#1} -gt 0 ] ; then
+  if [ -s $1 ] ; then
+      runList=`cat $1`
+  else
+      echo "<$1> does not seem to be a valid file"
+      exit 2
+  fi
+else
+    echo " no file provided"
+fi
 
 echo 
 echo 
 echo 
-echo 'Run numbers for processing:'
-cat $1
+echo 'Run numbers for processing'
+echo "${runList}"
+echo -e "list complete\n"
 
 #processing
 
-for i in $(cat $1); do
+for i in ${runList} ; do
 
 runnumber=$i
 
@@ -49,7 +83,7 @@ runnumber=$i
     for j in $(ls -r *.png); do
        cmsStage -f $j $WebDir/CMT/GLOBAL_$runnumber
     done 
-
+\
     rm *.html
     rm *.png    
 
@@ -73,24 +107,27 @@ runnumber=$i
 
 done
 
-cmsLs $HistoDir | grep root | awk  '{print $5}' | awk -F / '{print $10}' > rtmp
+echo "Get list of files in ${HistoDir}"
+#cmsLs $HistoDir | grep root | awk  '{print $5}' | awk -F / '{print $10}' > rtmp
+#cat rtmp | awk -F _ '{print $2}' | awk -F . '{print $1}' > _runlist_
+# Warning obtained on Oct 19, 2015:
+#   WARNING: this script (cmsLs) is deprecated and will be removed in Jan 2016
+#           Please use the 'eos ls' command directly.
 
-cat rtmp | awk -F _ '{print $2}' | awk -F . '{print $1}' > _runlist_
-rm rtmp
-echo 
-echo 
-echo 
-echo 'Run numbers:'
-cat _runlist_
+histoFiles=`${eos} ls $HistoDir | grep root | awk -F '_' '{print $2}' | awk -F '.' '{print $1}'`
+echo -e '\n\nRun numbers on EOS:'
+runList=`echo $histoFiles | tee _runlist_`
+echo "${runList}"
+echo -e "list complete\n"
 
 #making table
-
 
 # print header to index.html 
 echo `cat header_GLOBAL_EOS.txt`> index_draft.html
 
-#extract run numners
-for i in $(cat _runlist_); do
+#extract run numbers
+k=0
+for i in ${runList} ; do
  
 #runnumber=$(echo $i | sed -e 's/[^0-9]*//g')
 #runnumber=$(echo $i | awk -F 'run' '{print $2}'| awk -F '.' '{print $1}')
@@ -103,7 +140,19 @@ echo
 echo 'RUN number = '$runnumber
 
 # extract the date of file
-./das_client.py --query="run=${i} | grep run.beam_e,run.bfield,run.nlumis,run.lhcFill,run.delivered_lumi,run.duration,run.start_time,run.end_time" --limit=0 > tmp
+got=0
+if [ ${debug} -eq 2 ] ; then
+    rm -f tmp
+    cp ${DEBUG_DIR}/das_${runnumber}.txt tmp
+    if [ -s tmp ] ; then
+	got=1
+    else
+	echo "failed to use ${DEBUG_DIR} contents for ${runnumber}"
+    fi
+fi
+if [ ${got} -eq 0 ] ; then
+    ./das_client.py --query="run=${i} | grep run.beam_e,run.bfield,run.nlumis,run.lhcFill,run.delivered_lumi,run.duration,run.start_time,run.end_time" --limit=0 > tmp
+fi
 
 #cat tmp
 date=`cat tmp | awk '{print $7" "$8}'`
@@ -114,11 +163,18 @@ nL=`cat tmp | awk '{print $3}'`
 Fill=`cat tmp | awk '{print $4}'`
 dLumi=`cat tmp | awk '{print $5}'`
 D=`cat tmp | awk '{print $6}'`
+if [ ${debug} -eq 1 ] ; then cp tmp ${DEBUG_DIR}/das_${runnumber}.txt; fi
 rm tmp
 
-#cmsLs $HistoDir/Global_$i.root | awk '{print $3" "$4}'
+#echo 'ver 1'
+#${eos} ls $HistoDir/Global_$i.root
+#echo 'ver 2'
+#${eos} ls -l $HistoDir/Global_$i.root
+#old Date_obr=`${eos} ls -l $HistoDir/Global_$i.root | awk '{print $3" "$4}'`
 
-Date_obr=`cmsLs $HistoDir/Global_$i.root | awk '{print $3" "$4}'`
+fileinfo=`${eos} ls -l $HistoDir/Global_$i.root`
+Date_obr=`echo ${fileinfo} | awk '{print $6" "$7" "$8}'`
+echo "Date_obr=$Date_obr"
 
 # extract run type, data, time and number of events
 type='Cosmic'
@@ -145,7 +201,6 @@ echo 'RUN LHC Fill = '$Fill
 echo 'RUN Delivered Luminosity = '$dLumi
 echo 'RUN Date processing = '$Date_obr
 #echo 'RUN Comment = '$commentariy
-
 
 #adding entry to list of file index_draft.html
 let "raw = (k % 2) + 2"
@@ -175,19 +230,21 @@ done
 # print footer to index.html 
 echo `cat footer.txt`>> index_draft.html
 
-cmsStage -f index_draft.html $WebDir/CMT/index.html 
-rm index_draft.html
+cmsStage -f index_draft.html $WebDir/CMT/index.html
+status="$?"
+#rm index_draft.html
 
 # delete temp files
 
 rm *.root
 rm _runlist_
 
+echo "script done"
+
 # check exit code
-if [[ $? == 0 ]]; then
-  echo "Successfully done!"
+if [[ "${status}" == "0" ]]; then
+  echo "Successfully uploaded!"
 else
-  rm $dst
-  echo "ERROR: Processing failed!"
+  echo "ERROR: Uploading failed"
   exit 1
 fi
