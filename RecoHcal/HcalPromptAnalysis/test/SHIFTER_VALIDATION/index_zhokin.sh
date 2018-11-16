9877ca70895ad51b17249ae7f957ec1cedd784c9
Alexander <zhokin@mail.cern.ch>
2018-11-16 15:09:35 +0100

    AZ 16.11.2018

--- RecoHcal/HcalPromptAnalysis/test/SHIFTER_VALIDATION/index_zhokin.sh
+++ RecoHcal/HcalPromptAnalysis/test/SHIFTER_VALIDATION/index_zhokin.sh
@@ -179,7 +179,7 @@ cat index.html | head -n -1 > index_draft.html
 #for i in ${runListEOSall} ; do
 #let "k = k + 1"
 #done
-k=1840
+k=1953
 ########################################## type by hands number of new runs k=k-number:
 #let "k = k - 1"
 echo ' ================>>>    k in old list = '$k
