d9d76941ac37af543f373447d72f694231842557
Alexander <zhokin@mail.cern.ch>
2020-02-19 10:03:10 +0100

    AZ 19.02.2020

--- RecoHcal/HcalPromptAnalysis/test/SHIFTER_VALIDATION/GlobalRMT_EOS.sh
+++ RecoHcal/HcalPromptAnalysis/test/SHIFTER_VALIDATION/GlobalRMT_EOS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WebDir='/eos/cms/store/group/dpg_hcal/comm_hcal/www/HcalRemoteMonitoring'
-WebSite='https://cms-conddb-dev.cern.ch/eosweb/hcal/HcalRemoteMonitoring'
+WebSite='https://cms-conddb.cern.ch/eosweb/hcal/HcalRemoteMonitoring'
 HistoDir='/store/group/dpg_hcal/comm_hcal/www/HcalRemoteMonitoring/CMT/histos'
 eos='/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select'
 
@@ -170,34 +170,60 @@ for i in ${runList} ; do
     rm -rf ${local_WebDir}
     if [ ! -d ${local_WebDir} ] ; then mkdir ${local_WebDir}; fi
     for j in $(ls -r *.html); do
-	cat $j | sed 's#cms-cpt-software.web.cern.ch\/cms-cpt-software\/General\/Validation\/SVSuite#cms-conddb-dev.cern.ch\/eosweb\/hcal#g' \
+	cat $j | sed 's#cms-cpt-software.web.cern.ch\/cms-cpt-software\/General\/Validation\/SVSuite#cms-conddb.cern.ch\/eosweb\/hcal#g' \
 		> ${local_WebDir}/$j
     done
 
     cp *.png ${local_WebDir}
 #    cp HELP.html ${local_WebDir}
 
-    files=`cd ${local_WebDir}; ls`
-    #echo "GlobalRMT files=${files}"
 
-    if [ ${debug} -eq 0 ] ; then
-	eos mkdir $WebDir/GlobalRMT/GLOBAL_$runnumber
-	if [ ! $? -eq 0 ] ; then
-	    echo "GlobalRMT eos mkdir failed"
-	    exit 2
-	fi
-	for f in ${files} ; do
-	    echo "eoscp ${local_WebDir}/${f} $WebDir/GlobalRMT/GLOBAL_$runnumber/${f}"
-	    eoscp ${local_WebDir}/${f} $WebDir/GlobalRMT/GLOBAL_$runnumber/${f}
-	    if [ ! $? -eq 0 ] ; then
-		echo "GlobalRMT eoscp failed for ${f}"
-		exit 2
-	    fi
-	done
-    else
-        # debuging
-	echo "debugging: files are not copied to EOS"
-    fi
+#---------------111
+# first variant:
+#    mkdir $WebDir/GlobalRMT/GLOBAL_$runnumber
+#    cp *.png $WebDir/GlobalRMT/GLOBAL_$runnumber/.
+#    cp *.html $WebDir/GlobalRMT/GLOBAL_$runnumber/.
+# echo "cp  png and html file done"
+
+
+#---------------222
+# more simple second variant:
+scp -r ${local_WebDir} $WebDir/GlobalRMT/GLOBAL_$runnumber
+echo "cp -r for dir with png and html file done"
+
+
+
+
+
+#---------------old
+# instead old variant:
+
+
+#    files=`cd ${local_WebDir}; ls`
+    ####echo "GlobalRMT files=${files}"
+
+#    if [ ${debug} -eq 0 ] ; then
+#	eos mkdir $WebDir/GlobalRMT/GLOBAL_$runnumber
+#	if [ ! $? -eq 0 ] ; then
+#	    echo "GlobalRMT eos mkdir failed"
+#	    exit 2
+#	fi
+#	for f in ${files} ; do
+#	    echo "eoscp ${local_WebDir}/${f} $WebDir/GlobalRMT/GLOBAL_$runnumber/${f}"
+#	    eoscp ${local_WebDir}/${f} $WebDir/GlobalRMT/GLOBAL_$runnumber/${f}
+#	    if [ ! $? -eq 0 ] ; then
+#		echo "GlobalRMT eoscp failed for ${f}"
+#		exit 2
+#	    fi
+#	done
+#    else
+        #### debuging
+#	echo "debugging: files are not copied to EOS"
+#    fi
+#---------------
+#
+#   removing:
+#
 
     rm *.html
     rm *.png
